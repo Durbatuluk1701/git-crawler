45418036aa762e1722a270f780d2e78100095167
Will Thomas <30wthomas@gmail.com>
2023-12-19 14:59:49 -0600

    new type of summary stat

--- Summarizer.js
+++ Summarizer.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const DbFilePath = "./DB.json";
 const OutputValueDb = "./SummaryVals.json";
 const OutputFullDb = "./Summary.json";
+const OutputFullGroupedDb = "./SummaryGrouped.json";
 const OutputMiniDb = "./SummaryMini.json";
 const OutputStatsTxt = "./SummaryStats.txt";
 
@@ -33,40 +34,14 @@ const summarizeDb = (inpDb) => {
 };
 
 const groupKeys = (keys) => {
-  const shiftedChars = {
-    "!": "1",
-    "@": "2",
-    "#": "3",
-    $: "4",
-    "%": "5",
-    "^": "6",
-    "&": "7",
-    "*": "8",
-    "(": "9",
-    ")": "0",
-    _: "-",
-    "+": "=",
-    "{": "[",
-    "}": "]",
-    "|": "\\",
-    ":": ";",
-    '"': "'",
-    "<": ",",
-    ">": ".",
-    "?": "/",
-    "~": "`",
-  };
-
   const groupedKeys = {};
 
   keys.forEach((key) => {
     const normalizedKey = key.toLowerCase();
-    const baseKey = shiftedChars[normalizedKey] || normalizedKey;
-
-    if (!groupedKeys[baseKey]) {
-      groupedKeys[baseKey] = [key];
+    if (!groupedKeys[normalizedKey]) {
+      groupedKeys[normalizedKey] = [key];
     } else {
-      groupedKeys[baseKey].push(key);
+      groupedKeys[normalizedKey].push(key);
     }
   });
 
@@ -74,9 +49,83 @@ const groupKeys = (keys) => {
 };
 
 // NOTE: Some big issue with the sorting, since numbers are not valid JS idents they move to top
-const calcDbStats = (obj) => {
+const calcDbStats = (obj, debug = true) => {
   const totalChars = Object.keys(obj).reduce((acc, v) => acc + obj[v], 0);
-  console.log(`Total Chars Witnesses: ${totalChars}\n`);
+  debug && console.log(`Total Chars Witnesses: ${totalChars}\n`);
+  const filterUnprintables = Object.keys(obj).filter((key) => {
+    const jsonStrRep = JSON.stringify(key);
+    if (
+      jsonStrRep.includes("\\u") ||
+      jsonStrRep.includes("\\r") ||
+      jsonStrRep.includes("\\b") ||
+      jsonStrRep.includes("\\f") ||
+      jsonStrRep.includes("")
+    ) {
+      debug && console.log("FILTERED: ", jsonStrRep);
+      return false;
+    }
+    return true;
+  });
+  const groupedKeys = groupKeys(filterUnprintables);
+  // Get output
+  const groupedPrintableKeys = {};
+  Object.keys(groupedKeys).forEach((a) => {
+    let aVal = 0;
+    groupedKeys[a].forEach((val) => {
+      aVal += obj[val];
+    });
+    groupedPrintableKeys[a] = aVal;
+  });
+  const numPrintableChars = filterUnprintables.reduce(
+    (acc, v) => acc + obj[v],
+    0
+  );
+  debug && console.log(`Printable Chars Witnesses: ${numPrintableChars}\n`);
+  const retDb = {};
+  Object.entries(groupedPrintableKeys)
+    .sort(([ak, av], [bk, bv]) => bv - av)
+    .forEach(([k, v]) => {
+      retDb[k] = v / numPrintableChars;
+    });
+  return retDb;
+};
+
+const minifyDb = (inpDb) => {
+  const newDb = {};
+  Object.keys(inpDb).forEach((char) => {
+    newDb[char] = inpDb[char]["0"][char];
+  });
+  return newDb;
+};
+
+const statDbToPrint = (db) => {
+  let retStr = "";
+  Object.entries(db)
+    .sort(([ak, av], [bk, bv]) => bv - av)
+    .forEach(([k, v]) => {
+      const strVal = JSON.stringify(`Key: '${k}', Freq: ${v}`);
+      retStr += strVal.slice(1) + "\n";
+    });
+  return retStr;
+};
+
+const fullDb = summarizeDb(db);
+
+const mergeObjects = (obj1, obj2) => {
+  const result = { ...obj1 };
+
+  for (const key in obj2) {
+    if (result.hasOwnProperty(key)) {
+      result[key] += obj2[key];
+    } else {
+      result[key] = obj2[key];
+    }
+  }
+
+  return result;
+};
+
+const fullDbMerge = (obj) => {
   const filterUnprintables = Object.keys(obj).filter((key) => {
     const jsonStrRep = JSON.stringify(key);
     if (
@@ -92,42 +141,32 @@ const calcDbStats = (obj) => {
     return true;
   });
   const groupedKeys = groupKeys(filterUnprintables);
-  // Get output
-  const groupedPrintableKeys = Object.keys(groupedKeys).map((a) => {
-    let aVal = 0;
-    groupedKeys[a].forEach((val) => {
-      aVal += obj[val];
+  console.log("GK", groupedKeys);
+  const retDb = {};
+  Object.keys(groupedKeys).forEach((key) => {
+    const subDbList = groupedKeys[key];
+    console.log(subDbList);
+    retDb[key] = { "-3": {}, "-2": {}, "-1": {}, 0: {} };
+    subDbList.forEach((subDbKey) => {
+      // Clean the sub-db for each length
+      ["-3", "-2", "-1", "0"].forEach((dist) => {
+        retDb[key][dist] = mergeObjects(retDb[key][dist], obj[subDbKey][dist]);
+      });
     });
-    return [a, aVal];
-  });
-  const numPrintableChars = filterUnprintables.reduce(
-    (acc, v) => acc + obj[v],
-    0
-  );
-  console.log(`Printable Chars Witnesses: ${numPrintableChars}\n`);
-  let retStr = "";
-  groupedPrintableKeys
-    .sort((a, b) => b[1] - a[1])
-    .forEach(([k, v]) => {
-      retStr +=
-        // `Key: '${key}', Freq: ${obj[key] / totalChars}` + "\n";
-        JSON.stringify(`Key: '${k}', Freq: ${v / numPrintableChars}`) + "\n";
+    ["-3", "-2", "-1", "0"].forEach((dist) => {
+      retDb[key][dist] = calcDbStats(retDb[key][dist], false);
     });
-  return retStr;
-};
-
-const minifyDb = (inpDb) => {
-  const newDb = {};
-  Object.keys(inpDb).forEach((char) => {
-    newDb[char] = inpDb[char]["0"][char];
   });
-  return newDb;
+  return retDb;
 };
-
-const fullDb = summarizeDb(db);
 
 const minifiedDb = minifyDb(fullDb);
 
 fs.writeFileSync(OutputFullDb, JSON.stringify(fullDb, null, 2));
 fs.writeFileSync(OutputMiniDb, JSON.stringify(minifiedDb, null, 2));
-fs.writeFileSync(OutputStatsTxt, calcDbStats(minifiedDb));
+fs.writeFileSync(OutputStatsTxt, statDbToPrint(calcDbStats(minifiedDb)));
+
+fs.writeFileSync(
+  OutputFullGroupedDb,
+  JSON.stringify(fullDbMerge(fullDb), null, 2)
+);
