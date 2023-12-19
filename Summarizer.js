a8e563448f2ba3debf5bedea6f537e3dbe3d0dec
Will Thomas <30wthomas@gmail.com>
2023-12-19 12:13:53 -0600

    Upgrading the summarizer

--- Summarizer.js
+++ Summarizer.js
@@ -2,191 +2,63 @@ const fs = require("fs");
 
 const DbFilePath = "./DB.json";
 const OutputValueDb = "./SummaryVals.json";
-const OutputDb = "./Summary.json";
+const OutputFullDb = "./Summary.json";
+const OutputMiniDb = "./SummaryMini.json";
+const OutputStatsDb = "./SummaryStats.json";
 
 const db = JSON.parse(fs.readFileSync(DbFilePath));
 
-const allowedExtensions = [
-  "c",
-  "cpp",
-  "h",
-  "hpp",
-  "cc",
-  "cxx",
-  "hh",
-  "inl",
-  "ipp",
-  "java",
-  "py",
-  "pyc",
-  "pyd",
-  "pyo",
-  "pyw",
-  "pyx",
-  "pyi",
-  "pyz",
-  "js",
-  "jsx",
-  "mjs",
-  "html",
-  "htm",
-  "xhtml",
-  "css",
-  "scss",
-  "less",
-  "sass",
-  "rb",
-  "ru",
-  "rhtml",
-  "swift",
-  "php",
-  "php3",
-  "php4",
-  "php5",
-  "php7",
-  "phtml",
-  "pl",
-  "pm",
-  "t",
-  "pod",
-  "cgi",
-  "sh",
-  "bash",
-  "zsh",
-  "fish",
-  "go",
-  "r",
-  "R",
-  "Rmd",
-  "Rnw",
-  "Rhistory",
-  "Rprofile",
-  "kt",
-  "kts",
-  "ts",
-  "scala",
-  "sbt",
-  "rs",
-  "dart",
-  "lua",
-  "sql",
-  "ddl",
-  "dml",
-  "asm",
-  "s",
-  "hs",
-  "lhs",
-  "groovy",
-  "m",
-  "fs",
-  "fsx",
-  "fsi",
-  "bat",
-  "cmd",
-  "yaml",
-  "yml",
-  "json",
-  "xml",
-  "xsd",
-  "xsl",
-  "dts",
-  "dockerfile",
-  "md",
-  "markdown",
-  "clj",
-  "cljs",
-  "cljc",
-  "edn",
-  "ex",
-  "exs",
-  "erl",
-  "coffee",
-  "cr",
-  "jl",
-  "mm",
-  "vhd",
-  "vhdl",
-  "v",
-  "ps1",
-  "psm1",
-  "psd1",
-  "rkt",
-  "re",
-  "ml",
-  "mli",
-  "zig",
-  "kscript",
-  "tf",
-  "tfvars",
-  "tfstate",
-  "hx",
-  "pp",
-  "e",
-  "f",
-  "for",
-  "f90",
-  "f95",
-  "f03",
-  "f08",
-  "cls",
-  "graphql",
-  "gql",
-  "vi",
-  "ctl",
-  "lvclass",
-  "pl",
-  "pro",
-  "makefile",
-  "cbl",
-  "cob",
-  "toml",
-  "scm",
-  "hack",
-  "cfm",
-  "cfc",
-  "cfs",
-  "nim",
-  "nimble",
-  "gsp",
-  "dm",
-  "robot",
-  "ebnf",
-  "rei",
-  "idl",
-  "sol",
-];
-
-keysFilter = allowedExtensions.map((key) => db[key]).filter((x) => x);
-
-const objMerge = (oldDb, newDb) => {
-  Object.keys(newDb).forEach((key) => {
-    const newVal = newDb[key];
-    const curVal = oldDb[key];
-    oldDb[key] = curVal ? curVal + newVal : newVal;
+const summarizeDb = (inpDb) => {
+  const returnDb = {};
+  Object.keys(inpDb).forEach((ext) => {
+    const newVal = inpDb[ext];
+    Object.keys(newVal).forEach((char) => {
+      if (!returnDb[char]) {
+        returnDb[char] = { "-3": {}, "-2": {}, "-1": {}, 0: {} };
+      }
+      const extCharVal = newVal[char];
+      Object.keys(extCharVal).forEach((dist) => {
+        const extCharDistVal = extCharVal[dist];
+        Object.keys(extCharDistVal).forEach((char_2) => {
+          const newExtCharDistCharVal = extCharDistVal[char_2];
+          const curVal = returnDb[char][dist][char_2];
+          returnDb[char][dist][char_2] = curVal
+            ? curVal + newExtCharDistCharVal
+            : newExtCharDistCharVal;
+        });
+      });
+    });
   });
-  return oldDb;
+  return returnDb;
 };
 
-outDb = keysFilter.reduce(objMerge, {});
+// NOTE: Some big issue with the sorting, since numbers are not valid JS idents they move to top
+const calcDbStats = (obj) => {
+  const totalChars = Object.keys(obj).reduce((acc, v) => acc + obj[v], 0);
+  console.log(`Total Chars Witnesses: ${totalChars}\n`);
+  const retDb = {};
+  Object.keys(obj)
+    .sort((a, b) => obj[b] - obj[a])
+    .map((key) => {
+      retDb[key] = obj[key] / totalChars;
+    });
+  return retDb;
+};
 
-function sortObjectKeys(obj) {
-  const sortedKeys = Object.keys(obj).sort();
-  const sortedObject = Object.fromEntries(
-    sortedKeys.map((key) => [key, obj[key]])
-  );
-  return sortedObject;
-}
+const minifyDb = (inpDb) => {
+  const newDb = {};
+  Object.keys(inpDb).forEach((char) => {
+    newDb[char] = inpDb[char]["0"][char];
+  });
+  return newDb;
+};
 
-function sortObjectByNumericValue(obj) {
-  const sortedKeys = Object.keys(obj).sort((a, b) => obj[b] - obj[a]);
-  const sortedObject = Object.fromEntries(
-    sortedKeys.map((key) => [key, obj[key]])
-  );
-  return sortedObject;
-}
+const fullDb = summarizeDb(db);
 
-outDb = sortObjectKeys(outDb);
-outDb2 = sortObjectByNumericValue(outDb);
+const minifiedDb = minifyDb(fullDb);
 
-fs.writeFileSync(OutputDb, JSON.stringify(outDb, null, 2));
-fs.writeFileSync(OutputValueDb, JSON.stringify(outDb2, null, 2));
+const dbStats = calcDbStats(minifiedDb);
+
+fs.writeFileSync(OutputFullDb, JSON.stringify(fullDb, null, 2));
+fs.writeFileSync(OutputMiniDb, JSON.stringify(minifiedDb, null, 2));
+fs.writeFileSync(OutputStatsDb, JSON.stringify(dbStats, null, 2));
