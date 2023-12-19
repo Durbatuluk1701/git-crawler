0c8117e46ae34c8b79568bcabd6827d36adefd60
Will Thomas <30wthomas@gmail.com>
2023-12-19 13:12:49 -0600

    Updating summary output (grouping lower upper)

--- Summarizer.js
+++ Summarizer.js
@@ -4,7 +4,7 @@ const DbFilePath = "./DB.json";
 const OutputValueDb = "./SummaryVals.json";
 const OutputFullDb = "./Summary.json";
 const OutputMiniDb = "./SummaryMini.json";
-const OutputStatsDb = "./SummaryStats.json";
+const OutputStatsTxt = "./SummaryStats.txt";
 
 const db = JSON.parse(fs.readFileSync(DbFilePath));
 
@@ -32,17 +32,88 @@ const summarizeDb = (inpDb) => {
   return returnDb;
 };
 
+const groupKeys = (keys) => {
+  const shiftedChars = {
+    "!": "1",
+    "@": "2",
+    "#": "3",
+    $: "4",
+    "%": "5",
+    "^": "6",
+    "&": "7",
+    "*": "8",
+    "(": "9",
+    ")": "0",
+    _: "-",
+    "+": "=",
+    "{": "[",
+    "}": "]",
+    "|": "\\",
+    ":": ";",
+    '"': "'",
+    "<": ",",
+    ">": ".",
+    "?": "/",
+    "~": "`",
+  };
+
+  const groupedKeys = {};
+
+  keys.forEach((key) => {
+    const normalizedKey = key.toLowerCase();
+    const baseKey = shiftedChars[normalizedKey] || normalizedKey;
+
+    if (!groupedKeys[baseKey]) {
+      groupedKeys[baseKey] = [key];
+    } else {
+      groupedKeys[baseKey].push(key);
+    }
+  });
+
+  return groupedKeys;
+};
+
 // NOTE: Some big issue with the sorting, since numbers are not valid JS idents they move to top
 const calcDbStats = (obj) => {
   const totalChars = Object.keys(obj).reduce((acc, v) => acc + obj[v], 0);
   console.log(`Total Chars Witnesses: ${totalChars}\n`);
-  const retDb = {};
-  Object.keys(obj)
-    .sort((a, b) => obj[b] - obj[a])
-    .map((key) => {
-      retDb[key] = obj[key] / totalChars;
+  const filterUnprintables = Object.keys(obj).filter((key) => {
+    const jsonStrRep = JSON.stringify(key);
+    if (
+      jsonStrRep.includes("\\u") ||
+      jsonStrRep.includes("\\r") ||
+      jsonStrRep.includes("\\b") ||
+      jsonStrRep.includes("\\f") ||
+      jsonStrRep.includes("")
+    ) {
+      console.log("FILTERED: ", jsonStrRep);
+      return false;
+    }
+    return true;
+  });
+  const groupedKeys = groupKeys(filterUnprintables);
+  // Get output
+  const groupedPrintableKeys = Object.keys(groupedKeys).map((a) => {
+    let aVal = 0;
+    groupedKeys[a].forEach((val) => {
+      aVal += obj[val];
     });
-  return retDb;
+    return [a, aVal];
+  });
+  const numPrintableChars = filterUnprintables.reduce(
+    (acc, v) => acc + obj[v],
+    0
+  );
+  console.log(`Printable Chars Witnesses: ${numPrintableChars}\n`);
+  let retStr = "";
+  groupedPrintableKeys
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([k, v]) => {
+      retStr +=
+        // `Key: '${key}', Freq: ${obj[key] / totalChars}` + "\n";
+        JSON.stringify(`Key: '${k}', Freq: ${v / numPrintableChars}`) + "\n";
+    });
+  return retStr;
 };
 
 const minifyDb = (inpDb) => {
@@ -57,8 +128,6 @@ const fullDb = summarizeDb(db);
 
 const minifiedDb = minifyDb(fullDb);
 
-const dbStats = calcDbStats(minifiedDb);
-
 fs.writeFileSync(OutputFullDb, JSON.stringify(fullDb, null, 2));
 fs.writeFileSync(OutputMiniDb, JSON.stringify(minifiedDb, null, 2));
-fs.writeFileSync(OutputStatsDb, JSON.stringify(dbStats, null, 2));
+fs.writeFileSync(OutputStatsTxt, calcDbStats(minifiedDb));
